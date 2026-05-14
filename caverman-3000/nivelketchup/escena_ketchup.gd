a60bd131611a1bd3ffc25e13a56309db2f8db1d3
AnxietyFilledRaccoon <105757069+AnxietyFilledRaccoon@users.noreply.github.com>
2026-05-14 00:36:44 -0300

    Finalización de la base del nivel
    Se crea la base para el nivel de ketchup.

--- caverman-3000/nivelketchup/escena_ketchup.gd
+++ caverman-3000/nivelketchup/escena_ketchup.gd
@@ -1,37 +1,34 @@
 extends Node2D
 
-@export var hits_required : int = 10
+@export var hits_required : int = 20
 
 var current_hits : int = 0
 var exploded : bool = false
 
 func _ready():
-	$Ketchup/AnimationPlayer.animation_finished.connect(_on_animation_finished)
+	$Ketchup/AnimatedSprite2D.animation_finished.connect(_on_animation_finished)
 
-func _on_animation_finished(anim_name):
-	if anim_name == "explode":
+func _on_animation_finished():
+	if exploded:
 		go_to_victory()
 
 func _process(delta):
 	if Input.is_action_just_pressed("hit") and not exploded:
+		#$Ketchup/AnimatedSprite2D.play("idle")
 		register_hit()
 
-#Funciones
-
 func register_hit():
-	
 	current_hits += 1
 	print("Golpes:", current_hits)
-	
-	$Hand/AnimationPlayer.play("hit")
-	$Ketchup/AnimationPlayer.play("shake")
+	$Hand/AnimatedSprite2D.play("shake")
+	$Ketchup/AnimatedSprite2D.play("hit")
 	
 	if current_hits >= hits_required:
 		explode()
 
 func explode():
 	exploded = true
-	$Ketchup/AnimationPlayer.play("explode")
+	$Ketchup/AnimatedSprite2D.play("explode")
 
 func go_to_victory():
 	get_tree().change_scene_to_file("res://VictoryScreen.tscn")
