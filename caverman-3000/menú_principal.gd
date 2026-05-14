906d2bd70f183389449468eb9aa3e46c559d435a
AnxietyFilledRaccoon <105757069+AnxietyFilledRaccoon@users.noreply.github.com>
2026-05-14 00:52:32 -0300

    Función go_to_next_level
    Armé una función y la comenté para no arruinar tu codigo, basicamente para que podamos ir al nivel del ketchup directamente.

--- caverman-3000/menú_principal.gd
+++ caverman-3000/menú_principal.gd
@@ -5,10 +5,13 @@ func _on_play_pressed() -> void:
 	await get_tree().create_timer(1.0).timeout 
 	get_tree().change_scene_to_file("res://nivel1/juego1.tscn")
 
-
 func _on_opcion_pressed() -> void:
 	pass # Replace with function body.
 
-
 func _on_quitar_pressed() -> void:
 	get_tree().quit()
+
+# Hice esta funcion que basicamente pasaria al nivel del ketchup pero no quiero arruinarte el codigo metiendo mano
+# asi que usala cuando sientas que sea correcto, seria tecnicamente cuando termine el nivel 2.	
+#func go_to_next_level():
+#	get_tree().change_scene_to_file("res://nivelketchup/escena_ketchup.tscn")
