9a371bf6e7a094ae98cb5d76ddc02b9b924ea20d
FelipeFik11 <dedominicifelipe@gmail.com>
2026-06-04 20:39:54 -0300

    modificaciones del contador nivel guadaña

--- caverman-3000/nivelguadaña/pastolargo.gd
+++ caverman-3000/nivelguadaña/pastolargo.gd
@@ -4,7 +4,13 @@ class_name pasto
 var cut := false
 
 
+
 func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print("pasto cortado")
+			get_parent().sumar_corte()
+			queue_free()
 
 	if cut:
 		return
@@ -12,6 +18,7 @@ func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			
 
 			cut = true
 
