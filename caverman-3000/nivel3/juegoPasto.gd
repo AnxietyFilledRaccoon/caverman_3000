1b172da5e9706f980008c3bdf99b28020ebf7322
FelipeFik11 <dedominicifelipe@gmail.com>
2026-06-04 22:03:46 -0300

    update de juego pasto

--- caverman-3000/nivel3/juegoPasto.gd
+++ caverman-3000/nivel3/juegoPasto.gd
@@ -6,16 +6,22 @@ var pasto_cortado := 0
 func _ready():
 	pasto_total = get_tree().get_nodes_in_group("pasto").size()
 	actualizar_contador()
-
 func sumar_corte():
 	pasto_cortado += 1
+	$CanvasLayer/contador.text =str(pasto_cortado) +"/6"
+	if pasto_cortado ==6:
+		get_tree().change_scene_to_file("res://nivel1/juego1.tscn")
 	print("cortados:", pasto_cortado)
 	actualizar_contador() 
 	if pasto_cortado >= pasto_total:
 		finalizar()
+		
+		
 func finalizar():
 	print("Nivel completado")
-	#get_tree().change_scene_to_file("res://menú_principal.tscn")
+
+
+
 func actualizar_contador():
 	contador_label.text =str(pasto_cortado) +"/"+ str(pasto_total)
 
