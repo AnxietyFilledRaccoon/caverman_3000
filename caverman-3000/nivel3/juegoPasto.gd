0e4c839c0f739f518416901af2698162b9f5d837
FelipeFik11 <dedominicifelipe@gmail.com>
2026-06-04 21:20:31 -0300

    modificacion(2) nivel guadaña

--- caverman-3000/nivel3/juegoPasto.gd
+++ caverman-3000/nivel3/juegoPasto.gd
@@ -1,32 +1,23 @@
 extends Node2D
-var pasto_total := 0
+var pasto_total := 6
 var pasto_cortado := 0
 @onready var contador_label = $CanvasLayer/contador
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(contador_label)
-	pasto_total = get_tree(). get_nodes_in_group("pasto").size()
+	pasto_total = get_tree().get_nodes_in_group("pasto").size()
 	actualizar_contador()
-	
+
 func sumar_corte():
 	pasto_cortado += 1
 	print("cortados:", pasto_cortado)
 	actualizar_contador() 
 	if pasto_cortado >= pasto_total:
-		ganar()
-
+		finalizar()
+func finalizar():
+	print("Nivel completado")
+	#get_tree().change_scene_to_file("res://menú_principal.tscn")
 func actualizar_contador():
 	contador_label.text =str(pasto_cortado) +"/"+ str(pasto_total)
-func ganar():
-	print("nivel completado")
-	
-	#get_tree().change_scene_to_file()
-		
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 func _on_reiniciar_pressed() -> void:
