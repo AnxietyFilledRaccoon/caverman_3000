b8b68cb8047eb7a398da38b45f86926bf996e7ae
Leonardo Maidana <leo-lomas1@hotmail.com>
2026-06-07 05:56:44 -0300

    Nivel terminado
    Mecanica  hecha, faltan assets y estaria

--- caverman-3000/nivel7/juego_7.gd
+++ caverman-3000/nivel7/juego_7.gd
@@ -1,7 +1,7 @@
 extends Node2D
 #editor de nodos
 @onready var player7 = $Playerlvl7
-@onready var productos = $Productos
+@onready var productos = $Estanteria/Productos
 @onready var camion = $Camion
 @onready var timer_juego = $TimerJuego
 @onready var label_tiempo = $Ui/Control/LabelTiempo
@@ -11,15 +11,18 @@ extends Node2D
 #productos
 @onready var total_productos = 5
 @onready var productos_restantes = 5
+@onready var productos_nodos = $Estanteria/Productos.get_children()
+@onready var productos_total = $Playerlvl7/Productos
+@onready var productos_entregados_nodos = $Camion/ProductosEntregados.get_children()
 #jugabilidad
 var tiene_producto = false
 var juego_activo = true
 var en_movimiento = false
-var tiempo_total = 6
+var tiempo_total = 7
 
 func _ready():
 
-	player7.position.x = productos.position.x
+	player7.position.x = (productos.position.x + camion.position.x) / 2.0
 
 	timer_juego.wait_time = tiempo_total
 	timer_juego.start()
@@ -29,7 +32,12 @@ func _ready():
 	if !timer_juego.timeout.is_connected(_tiempo_agotado):
 		timer_juego.timeout.connect(_tiempo_agotado)
 	
+	productos_total.visible = false
+
 	_actualizar_ui()
+	_actualizar_productos_visuales()
+
+
 
 
 func _process(_delta: float) -> void:
@@ -50,20 +58,24 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("move_left"):
 
 		if !tiene_producto:
-			_mover_player(productos.position.x, _al_llegar_productos)
+			_mover_player(productos.position.x, _al_llegar_productos, true)
 	
 	if event.is_action_pressed("move_right"):
 		if tiene_producto:
-			_mover_player(camion.position.x, _al_llegar_camion)
+			_mover_player(camion.position.x, _al_llegar_camion, false)
 
-func _mover_player(destino_x, callback):
+func _mover_player(destino_x, callback, mirar_izquierda) -> void:
 
 	en_movimiento = true
+
+	var sprites = player7.find_children("*", "AnimatedSprite2D", true, false)
+	if sprites.size() > 0:
+		sprites[0].flip_h = mirar_izquierda
 
 	var tween = create_tween()
 	tween.set_ease(Tween.EASE_IN_OUT)
 	tween.set_trans(Tween.TRANS_SINE)
-	tween.tween_property(player7, "position:x", destino_x, 0.3)
+	tween.tween_property(player7, "position:x", destino_x, 0.4)
 	tween.tween_callback(func():
 		en_movimiento = false
 		callback.call()
@@ -72,12 +84,21 @@ func _mover_player(destino_x, callback):
 func _al_llegar_productos() -> void:
 	if productos_restantes > 0:
 		tiene_producto = true
+		productos_total.visible = true
+		_actualizar_productos_visuales()
 		_actualizar_ui()
+
 
 func _al_llegar_camion() -> void:
 	tiene_producto = false
 	productos_restantes -= 1
+	productos_total.visible = false
 	_actualizar_ui()
+	_actualizar_productos_visuales()
+	
+	var entregados = total_productos - productos_restantes - 1
+	if entregados >= 0 and entregados < productos_entregados_nodos.size():
+		productos_entregados_nodos[entregados].visible = true
 	
 	if productos_restantes <= 0:
 		_fin_juego(true)
@@ -85,6 +106,10 @@ func _al_llegar_camion() -> void:
 #ui
 func _actualizar_ui() -> void:
 	label_p_restantes.text = "Productos restantes: %d" % productos_restantes
+
+func _actualizar_productos_visuales() -> void:
+	for i in range(productos_nodos.size()):
+		productos_nodos[i].visible = i < productos_restantes
 
 func _tiempo_agotado() -> void:
 	_fin_juego(false)
@@ -96,7 +121,7 @@ func _fin_juego(gano: bool) -> void:
 	if gano:
 		await get_tree().create_timer(2.0).timeout
 		label_resultados.text = "¡Entregaste todos los productos!"
-		get_tree().change_scene_to_file("res://nivel8/nivel8.tscn")
+		get_tree().change_scene_to_file("res://nivel8/juego_8.tscn")
 	else:
 		await get_tree().create_timer(1.0).timeout
 		label_resultados.text = "¡Perdiste!" + "\n" + "se termino el tiempo"
