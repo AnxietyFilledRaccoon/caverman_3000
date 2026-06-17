281fdcb5b8b4876abd8572fab437af32c6479dac
leonardodani369-svg <leonardodani369@gmail.com>
2026-06-17 16:11:02 -0300

    mejoras nivel3
    ...

--- caverman-3000/menú_principal.gd
+++ caverman-3000/menú_principal.gd
@@ -5,7 +5,7 @@ func _on_play_pressed() -> void:
 	await get_tree().create_timer(1.0).timeout 
 	TransicionManager.cambiar_nivel(
 	"INTRO",
-	"Usa Cruceta","")# puedo agregar en el espacio libre la siguiente pantalla
+	preload ("res://asets/Fondos y otras escenas/Botones/Cruceta1.png"),"")# puedo agregar en el espacio libre la siguiente pantalla
 	#pero decidi que me de unos segundos antes de ejecutar el siguiente nivel, por eso lo hidce asi
 	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://Nivel0/nivelIntro.tscn")
