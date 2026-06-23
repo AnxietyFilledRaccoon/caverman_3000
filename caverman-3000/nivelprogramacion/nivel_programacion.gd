96f2177b919e0dbd3a81631c0fedf596fc442d5c
AnxietyFilledRaccoon <105757069+AnxietyFilledRaccoon@users.noreply.github.com>
2026-06-23 02:39:42 -0300

    Implementacion de codigo
    Se implementó el codigo.

--- caverman-3000/nivelprogramacion/nivel_programacion.gd
+++ caverman-3000/nivelprogramacion/nivel_programacion.gd
@@ -3,35 +3,32 @@ extends Node2D
 @export var hits_required : int = 20
 
 var current_hits : int = 0
-var exploded : bool = false
+var codeados : bool = false
 
 
 func _ready():
-	$Ketchup/AnimatedSprite2D.animation_finished.connect(_on_animation_finished)
+	$personaje/AnimatedSprite2D.animation_finished.connect(_on_animation_finished)
 
 func _on_animation_finished():
-	if exploded:
+	if codeado:
 		go_to_victory()
 
 func _process(delta):
-	if Input.is_action_just_pressed("hit") and not exploded:
-		#$Ketchup/AnimatedSprite2D.play("idle")
+	if Input.is_action_just_pressed("hit") and not codeado:
 		register_hit()
 
 func register_hit():
 	current_hits += 1
 	print("Golpes:", current_hits)
-	$Hand/AnimatedSprite2D.play("shake")
-	$Ketchup/AnimatedSprite2D.play("hit")
+	$personaje/AnimatedSprite2D.play("hit")
+	$computadora/AnimatedSprite2D.play("programanding")
 	
 	if current_hits >= hits_required:
-		
-		explode()
-		
-
-func explode():
-	exploded = true
-	$Ketchup/AnimatedSprite2D.play("explode")
+		codeado()
+	
+func codeado():
+	codeados = true
+	$computadora/AnimatedSprite2D.play("programanding")
 
 
 func go_to_victory():
@@ -39,7 +36,7 @@ func go_to_victory():
 	if eventos:
 		eventos.nivel_ganado()
 		await get_tree().create_timer(2.0).timeout
-	get_tree().change_scene_to_file("res://nivelketchup/VictoryScreen.tscn")
+	get_tree().change_scene_to_file("res://nivel_Programacion/escena_Programacion_Victoria.tscn")
 
 
 func _on_reiniciar_pressed() -> void:
