5f3c8f8291b0d50d01f403ce239c7c7a19c70c80
AnxietyFilledRaccoon <105757069+AnxietyFilledRaccoon@users.noreply.github.com>
2026-06-23 15:15:32 -0300

    Ajustes de programación
    Se ajustaron lineas de codigo para que el nivel funcione.

--- caverman-3000/nivelprogramacion/nivel_programacion.gd
+++ caverman-3000/nivelprogramacion/nivel_programacion.gd
@@ -7,29 +7,28 @@ var codeados : bool = false
 
 
 func _ready():
-	$personaje/AnimatedSprite2D.animation_finished.connect(_on_animation_finished)
+	$personaje.animation_finished.connect(_on_animation_finished)
 
 func _on_animation_finished():
-	if codeado:
+	if codeados:
 		go_to_victory()
 
 func _process(delta):
-	if Input.is_action_just_pressed("hit") and not codeado:
+	if Input.is_action_just_pressed("hit") and not codeados:
 		register_hit()
 
 func register_hit():
 	current_hits += 1
-	print("Golpes:", current_hits)
-	$personaje/AnimatedSprite2D.play("hit")
-	$computadora/AnimatedSprite2D.play("programanding")
-	
+	#print("Golpes:", current_hits)
+	$personaje.play("hit")
+	$computadora.play("programanding")
+
 	if current_hits >= hits_required:
 		codeado()
-	
+
 func codeado():
 	codeados = true
 	$computadora/AnimatedSprite2D.play("programanding")
-
 
 func go_to_victory():
 	var eventos = get_node_or_null("eventos")
@@ -38,9 +37,6 @@ func go_to_victory():
 		await get_tree().create_timer(2.0).timeout
 	get_tree().change_scene_to_file("res://nivel_Programacion/escena_Programacion_Victoria.tscn")
 
-
 func _on_reiniciar_pressed() -> void:
-	# Quita la pausa por si el juego estaba pausado al perder
-	get_tree().paused = false 
-	# Recarga el nivel actual
+	get_tree().paused = false
 	get_tree().reload_current_scene()
