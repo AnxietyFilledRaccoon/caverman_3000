65101a9057ccfc156a7b94245ced0c6107bba9df
AnxietyFilledRaccoon <105757069+AnxietyFilledRaccoon@users.noreply.github.com>
2026-06-23 18:27:25 -0300

    Implementacion de assets
    Se reemplazaron los placeholders.

--- caverman-3000/nivelprogramacion/nivel_programacion.gd
+++ caverman-3000/nivelprogramacion/nivel_programacion.gd
@@ -8,6 +8,8 @@ var codeados : bool = false
 
 func _ready():
 	$personaje.animation_finished.connect(_on_animation_finished)
+	print($personaje)
+	print($computadora)
 
 func _on_animation_finished():
 	if codeados:
@@ -16,27 +18,36 @@ func _on_animation_finished():
 func _process(delta):
 	if Input.is_action_just_pressed("hit") and not codeados:
 		register_hit()
+		animated_sprites(true)
+	if Input.is_action_just_released("hit"):
+		animated_sprites(false)
 
 func register_hit():
 	current_hits += 1
-	#print("Golpes:", current_hits)
-	$personaje.play("hit")
-	$computadora.play("programanding")
+	print("Golpes:", current_hits)
+	
+func animated_sprites(active: bool):
+	if active:
+		$personaje.frame = 1
+		$computadora.frame = 1
+	else:
+		$personaje.frame = 0
+		$computadora.frame = 0
 
 	if current_hits >= hits_required:
 		codeado()
 
 func codeado():
 	codeados = true
-	$computadora/AnimatedSprite2D.play("programanding")
-
+	$computadora.play("win")
+	
 func go_to_victory():
 	var eventos = get_node_or_null("eventos")
 	if eventos:
 		eventos.nivel_ganado()
 		await get_tree().create_timer(2.0).timeout
 	get_tree().change_scene_to_file("res://nivel_Programacion/escena_Programacion_Victoria.tscn")
-
+	
 func _on_reiniciar_pressed() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()
